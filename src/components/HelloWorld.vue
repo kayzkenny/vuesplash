f752df8a60c50e41ffff4ee094d56123a81e7904
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-02-28 19:08:43 +0100

    demo complete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-toolbar class="px-4">
+      <v-toolbar-title>Unsplash Demo</v-toolbar-title>
+      <v-form @submit.prevent="searchQuery">
+        <v-text-field
+          class="mx-4"
+          flat
+          full-width
+          hide-details
+          placeholder="search"
+          prepend-inner-icon="mdi-magnify"
+          solo
+          validate-on-blur
+          @change="searchQuery"
+          v-model="search"
+        ></v-text-field>
+      </v-form>
+    </v-toolbar>
+    <!-- <v-progress-linear v-if="isLoading" indeterminate color="green"></v-progress-linear> -->
+    <v-container>
+      <v-card v-if="search" class="my-4 pa-4">Search Results for "{{ search }}"</v-card>
+      <v-row v-if="isLoading">
+        <v-col cols="4" v-for="n in 6" :key="n">
+          <v-sheet
+            :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+            class="px-3 pt-3 pb-3"
+          >
+            <v-skeleton-loader class="mx-auto" max-width="300" type="card"></v-skeleton-loader>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="4" v-for="splash in unsplash" :key="splash.id" align-self="center">
+          <v-card max-width="360" @click="showDialog(splash)">
+            <v-img
+              class="white--text align-end"
+              :height="450"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              :src="splash.urls.small"
+            >
+              <span class="white--text title font-weight-light pl-4">{{ splash.user.name }}</span>
+              <p class="white--text subtitle-2 font-weight-light pl-4">{{ splash.user.location }}</p>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-dialog v-model="dialog" width="800">
+        <v-card>
+          <v-img
+            class="white--text align-end"
+            :height="450"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            :src="dialogSplash.urls.full"
+            :lazy-src="dialogSplash.urls.small"
+          >
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+          <v-card-text class="mt-6">
+            <span class="headline pl-4">{{ dialogSplash.user.name }}</span>
+            <p class="subtitle-2 pl-4 font-weight-bold">{{ dialogSplash.user.location }}</p>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  inject: ["theme"],
+  data: () => ({
+    unsplash: null,
+    isLoading: true,
+    search: "",
+    dialog: false,
+    isActive: false,
+    dialogSplash: {
+      urls: {
+        full: ""
+      },
+      user: {
+        name: "",
+        location: ""
+      }
+    }
+  }),
+  methods: {
+    showDialog(splash) {
+      this.dialogSplash = splash;
+      this.dialog = true;
+      // console.log(splash);
+    },
+    searchQuery() {
+      this.isLoading = true;
+      fetch(
+        `https://api.unsplash.com/search/photos?page=1&query=${this.search}&client_id=N8SLkZagem7GWVCHDbRDJHXBIusgk9-w0EHS2t5hADE`
+      )
+        .then(response => {
+          this.searchPlaceholder = `Search Results for "${this.search}"`;
+          this.isLoading = false;
+          return response.json();
+        })
+        .then(data => {
+          this.unsplash = data.results;
+          console.log(data);
+        });
+    }
+  },
+  created() {
+    fetch(
+      "https://api.unsplash.com/search/photos?page=1&query=africa&client_id=N8SLkZagem7GWVCHDbRDJHXBIusgk9-w0EHS2t5hADE"
+    )
+      .then(response => {
+        this.isLoading = false;
+        return response.json();
+      })
+      .then(data => {
+        this.unsplash = data.results;
+        console.log(data);
+      });
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
