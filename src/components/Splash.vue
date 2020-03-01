ac650db407ace18fb322fb7d67e9f37d9d0ca915
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-03-01 15:03:13 +0100

    added pagination support

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -18,9 +18,9 @@
       </v-form>
     </v-toolbar>
     <v-container>
-      <v-card v-if="search" class="my-4 pa-4">Search Results for "{{ search }}"</v-card>
+      <v-card v-if="search" class="my-4 pa-4">Search results for "{{ search }}"</v-card>
       <v-row v-if="isLoading">
-        <v-col cols="4" v-for="n in 6" :key="n">
+        <v-col cols="3" v-for="n in 6" :key="n">
           <v-sheet
             :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
             class="px-3 pt-3 pb-3"
@@ -29,12 +29,12 @@
           </v-sheet>
         </v-col>
       </v-row>
-      <v-row>
-        <v-col cols="4" v-for="splash in unsplash" :key="splash.id" align-self="center">
+      <v-row v-if="!isLoading">
+        <v-col cols="3" v-for="splash in unsplash" :key="splash.id" align-self="center">
           <v-card max-width="360" @click="showDialog(splash)">
             <v-img
               class="white--text align-end"
-              :height="450"
+              :height="360"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               :src="splash.urls.small"
             >
@@ -42,6 +42,20 @@
               <p class="white--text subtitle-2 font-weight-light pl-4">{{ splash.user.location }}</p>
             </v-img>
           </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" v-if="!isLoading">
+        <v-col cols="8">
+          <v-container class="max-width">
+            <v-pagination
+              v-model="page"
+              class="my-4"
+              :length="total_pages"
+              @input="input"
+              @next="next"
+              @previous="prev"
+            ></v-pagination>
+          </v-container>
         </v-col>
       </v-row>
       <v-dialog v-model="dialog" width="800">
@@ -74,9 +88,15 @@ export default {
   name: "HelloWorld",
   inject: ["theme"],
   data: () => ({
-    unsplash: null,
+    unsplash: [],
     isLoading: true,
     search: "",
+    endpoint: "https://api.unsplash.com/search/photos",
+    client_id: "N8SLkZagem7GWVCHDbRDJHXBIusgk9-w0EHS2t5hADE",
+    page: 1,
+    total: 10,
+    total_pages: 1,
+    defaultSearch: "africa",
     dialog: false,
     isActive: false,
     dialogSplash: {
@@ -90,14 +110,23 @@ export default {
     }
   }),
   methods: {
+    /**
+     * Show the dialog and set its data
+     * @function
+     * @param {object} splash - the selected picture
+     */
     showDialog(splash) {
       this.dialogSplash = splash;
       this.dialog = true;
     },
+    /**
+     * Fetch photos with search string and assign to unsplash property
+     * @function
+     */
     searchQuery() {
       this.isLoading = true;
       fetch(
-        `https://api.unsplash.com/search/photos?page=1&query=${this.search}&client_id=N8SLkZagem7GWVCHDbRDJHXBIusgk9-w0EHS2t5hADE`
+        `${this.endpoint}?page=${this.page}&query=${this.search}&client_id=${this.client_id}`
       )
         .then(response => {
           this.searchPlaceholder = `Search Results for "${this.search}"`;
@@ -105,17 +134,53 @@ export default {
           return response.json();
         })
         .then(data => (this.unsplash = data.results));
+    },
+    /**
+     * Fetch photos with default search string and assign to unsplash property
+     * @function
+     */
+    defaultSearchQuery() {
+      fetch(
+        `${this.endpoint}?page=${this.page}&query=${this.defaultSearch}&client_id=${this.client_id}`
+      )
+        .then(response => {
+          this.isLoading = false;
+          return response.json();
+        })
+        .then(data => {
+          this.unsplash = data.results;
+          this.total = data.total;
+          this.total_pages = data.total_pages;
+        });
+    },
+    /**
+     * Called when the next button is clicked on v-pagination
+     * @function
+     */
+    next() {
+      this.search ? this.searchQuery() : this.defaultSearchQuery();
+    },
+    /**
+     * Called when the previous button is clicked on v-pagination
+     * @function
+     */
+    prev() {
+      this.search ? this.searchQuery() : this.defaultSearchQuery();
+    },
+    /**
+     * Called when the input button is clicked on v-pagination
+     * @function
+     */
+    input() {
+      this.search ? this.searchQuery() : this.defaultSearchQuery();
     }
   },
+  /**
+   * Fetch photos with default search and assign to unsplash property
+   * @function
+   */
   created() {
-    fetch(
-      "https://api.unsplash.com/search/photos?page=1&query=africa&client_id=N8SLkZagem7GWVCHDbRDJHXBIusgk9-w0EHS2t5hADE"
-    )
-      .then(response => {
-        this.isLoading = false;
-        return response.json();
-      })
-      .then(data => (this.unsplash = data.results));
+    this.defaultSearchQuery();
   }
 };
 </script>
